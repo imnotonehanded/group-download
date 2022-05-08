5e897ce10bd8b86b928015ce824fbf5bbb70bb32
imnotonehanded <imnotonehanded@gmail.com>
2022-05-08 09:44:39 -0400

    added start fun

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ import re
 import requests
 import os
 import colorama
-ID = input("Enter ID: ")
-# root = ET.fromstring(country_data_as_string)
-def Find(string):
-    # findall() has been used
-    # with valid conditions for urls in string
+
+def find(string):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex, string)
     return [x[0] for x in url]
@@ -24,26 +21,34 @@ def deEmojify(text):
     return regrex_pattern.sub(r'', text)
 
 
-# file = open('shirt.png', 'x')
-for line in urllib.request.urlopen('https://assetdelivery.roblox.com/v1/asset/?id={}'.format(ID)):
-    # if str(line).find("<url>http://www.roblox.com/asset/?id="):
-    # print(Find(line.decode('iso8859-1'))[0][0:34])
-    line.decode('iso8859-1')
-    try:
+def getShirt(id):
+    for line in urllib.request.urlopen(f'https://assetdelivery.roblox.com/v1/asset/?id={id}'):
+        line.decode('iso8859-1')
+        try:
+            if find(line.decode('iso8859-1'))[0][0:32] == "http://www.roblox.com/asset/?id=":
+                file = open(requests.get(f'http://api.roblox.com/Marketplace/ProductInfo?assetId={id}').json()[
+                                'Name'] + ".png", "x")
+                urllib.request.urlretrieve(
+                    'https://assetdelivery.roblox.com/v1/asset/?id={}'.format(find(line.decode('iso8859-1'))[0][32::]),
+                    os.path.realpath(file.name))
+                print(colorama.Fore.GREEN+"[*] Downloaded!")
+        except Exception as e:
+            continue
 
-        #print(Find(line.decode('iso8859-1')))
-        if Find(line.decode('iso8859-1'))[0][0:32] == "http://www.roblox.com/asset/?id=":
-            # print("done1")
-            file = open(requests.get('http://api.roblox.com/Marketplace/ProductInfo?assetId={}'.format(ID)).json()[
-                            'Name'] + ".png", "x")
-            # file = open('shirt.png', 'x')
-            # print(file.name+" file")
-            urllib.request.urlretrieve(
-                'https://assetdelivery.roblox.com/v1/asset/?id={}'.format(Find(line.decode('iso8859-1'))[0][32::]),
-                os.path.realpath(file.name))
-            print(colorama.Fore.GREEN+"[*] Downloaded!")
-            # print(Find(line.decode('iso8859-1'))[0][32::])
-    except Exception as e:
-        #print(e
-        continue
-    # print(line.decode('iso8859-1')[line.decode('iso8859-1').find("<url>http://www.roblox.com/asset/?id=")::])
+def start():
+    print("""                                                                                                                    
+                                                                                                                    
+ $$$$$$\   $$$$$$\   $$$$$$\  $$\   $$\  $$$$$$\         $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  
+$$  __$$\ $$  __$$\ $$  __$$\ $$ |  $$ |$$  __$$\       $$  __$$\ $$  __$$\  \____$$\ $$  __$$\ $$  __$$\ $$  __$$\ 
+$$ /  $$ |$$ |  \__|$$ /  $$ |$$ |  $$ |$$ /  $$ |      $$ |  \__|$$$$$$$$ | $$$$$$$ |$$ /  $$ |$$$$$$$$ |$$ |  \__|
+$$ |  $$ |$$ |      $$ |  $$ |$$ |  $$ |$$ |  $$ |      $$ |      $$   ____|$$  __$$ |$$ |  $$ |$$   ____|$$ |      
+\$$$$$$$ |$$ |      \$$$$$$  |\$$$$$$  |$$$$$$$  |      $$ |      \$$$$$$$\ \$$$$$$$ |$$$$$$$  |\$$$$$$$\ $$ |      
+ \____$$ |\__|       \______/  \______/ $$  ____/       \__|       \_______| \_______|$$  ____/  \_______|\__|      
+$$\   $$ |                              $$ |                                          $$ |                          
+\$$$$$$  |                              $$ |                                          $$ |                          
+ \______/                               \__|                                          \__|                          """)
+
+    print("\n [1] Download Whole Group\n[2] Download Individual Clothing\n[3] Download List of Clothing")
+    choice = input("Enter Choice: ")
+
+start()
